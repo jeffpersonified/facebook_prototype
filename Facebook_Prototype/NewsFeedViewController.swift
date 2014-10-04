1e1393039c4d6e1aaefed6f541faa3cc41c84099
Jeff Smith <jeff@thoughtbot.com>
2014-10-04 13:37:47 -0700

    add tabs

---
+++ Facebook_Prototype/NewsFeedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NewsFeedViewController.swift
+//  Facebook_Prototype
+//
+//  Created by Jeff Smith on 10/4/14.
+//  Copyright (c) 2014 Jeff Smith. All rights reserved.
+//
+
+import UIKit
+
+class NewsFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
