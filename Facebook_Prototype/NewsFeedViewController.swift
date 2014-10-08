8c66d045430f9219d4bcc493ef42941086775671
Jeff Smith <jeff@thoughtbot.com>
2014-10-07 21:52:50 -0400

    add images, photo controller, segue, and actions

--- Facebook_Prototype/NewsFeedViewController.swift
+++ Facebook_Prototype/NewsFeedViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class NewsFeedViewController: UIViewController {
 
+    // Set local outlets and variables
+    @IBOutlet weak var newsFeedScrollView: UIScrollView!
+    @IBOutlet weak var newsFeedImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 0.83, green: 0.84, blue: 0.86, alpha: 1)
+        newsFeedScrollView.contentSize = newsFeedImageView.frame.size
+        
 
     }
+    
+    @IBAction func onPhotoGestureRecognizer(sender: UITapGestureRecognizer) {
+        println("yo")
+        
+    }
+
     
 }
---
+++ Facebook_Prototype/PhotoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoViewController.swift
+//  Facebook_Prototype
+//
+//  Created by Jeff Smith on 10/7/14.
+//  Copyright (c) 2014 Jeff Smith. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
